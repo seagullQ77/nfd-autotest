c6249f5e77a30f10cba7e03f44813a0c9e8646cf
xpson <sunxiaopeng@nongfadai.com>
2016-07-12 19:45:33 +0800

    修改

--- targetEditorTool/targetEditor.py
+++ targetEditorTool/targetEditor.py
@@ -19,19 +19,18 @@ try:
     conn = MySQLdb.connect(host='112.74.197.103', user='SecureTeam', passwd='Aqdy(%@#', port=9527,charset="utf8")
     cur = conn.cursor()
     conn.select_db('omega')
-    cur.execute('select target_status from withdrawal_split_target WHERE target_code=%s'%targetCode)
-    result=None
+    cur.execute("select target_status from withdrawal_split_target WHERE target_code='%s'"%targetCode)
     result = cur.fetchone()
     if result==('DFB',):
-        cur.execute("update withdrawal_split_target set target_status='DFK',target_title='%s',target_p2p_value_date='%s',target_full_time='%s' WHERE target_code=%s"%(targetTitle,p2pValueDate,fullTime,targetCode))
-        print '编辑成功!','标的:%s'%targetCode,'p2p起息日：%s'%p2pValueDate,"p2p满标时间:%s"%fullTime
+        cur.execute("update withdrawal_split_target set target_status='DFK',target_title='%s',target_p2p_value_date='%s',target_full_time='%s' WHERE target_code='%s'"%(targetTitle,p2pValueDate,fullTime,targetCode))
+        print 'success!','targetCode:%s'%targetCode,'p2pStartInterest:%s'%p2pValueDate,"p2pFullTime:%s"%fullTime
     else:
-        print "没有改成功!标的编号错啦？标的状态是："+str(result)
+        print "fail!target status is:"+str(result)
     conn.commit()
     cur.close()
     conn.close()
 
 except MySQLdb.Error, e:
-    print '修改失败！'
+    print 'fail!'
     print "Mysql Error %d: %s" % (e.args[0], e.args[1])
-print '任意键退出！'
+input('Anykey to quit!')
