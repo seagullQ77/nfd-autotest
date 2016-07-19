b7cbe73a62242c9a395ff016a72ffaa2b2ed400d
xpson <sunxiaopeng@nongfadai.com>
2016-07-19 12:52:30 +0800

    绕过p2p设置FKCG

--- targetEditorTool/targetEditor.py
+++ targetEditorTool/targetEditor.py
@@ -23,9 +23,28 @@ try:
     result = cur.fetchone()
     if result==('DFB',):
         cur.execute("update withdrawal_split_target set target_status='DFK',target_title='%s',target_p2p_value_date='%s',target_full_time='%s' WHERE target_code='%s'"%(targetTitle,p2pValueDate,fullTime,targetCode))
+        conn.commit()
+        print 'target status has been changed as DFK! Wait for FKCG now, count down！'
+        print result
+        while result !='HKZ':
+            result=None
+            cur.execute("select target_status from withdrawal_split_target WHERE target_title='%s'" % targetTitle)
+            result = cur.fetchone()[0]
+            time.sleep(2)
+            print '*',
+            cur.close()
+            conn.close()
+            cur=conn.cursor()
+            conn = MySQLdb.connect(host='112.74.197.103', user='SecureTeam', passwd='Aqdy(%@#', port=9527,charset="utf8")
+            cur = conn.cursor()
+            conn.select_db('omega')
+        print 'Pay status is FKCG! Go on testing!'
+        cur.execute("update withdrawal_split_target_lend set pay_status='FKCG',pay_desc='done',pay_time='%s' WHERE target_code='%s'" % (fullTime, targetCode))
+
         print 'success!','targetCode:%s'%targetCode,'p2pStartInterest:%s'%p2pValueDate,"p2pFullTime:%s"%fullTime
     else:
-        print "fail!target status is:"+str(result)
+        print "Seemed not OK! target status is:"+str(result)
+    cur.execute("update withdrawal_split_target_lend set pay_status='FKCG',pay_desc='done',pay_time='%s' WHERE target_code='%s'" %(fullTime,targetCode))
     conn.commit()
     cur.close()
     conn.close()
