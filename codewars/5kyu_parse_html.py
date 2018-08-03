5cf76eb6d3db3b8ecfea6443481b2a7ff4ccf5e2
seagullQ <qinhaiou77@163.com>
2018-08-03 15:14:21 +0800

    add tips

--- codewars/5kyu_parse_html.py
+++ codewars/5kyu_parse_html.py
@@ -1,3 +1,4 @@
+#解析codewars排行榜网页数据
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 URL = 'https://www.codewars.com/users/leaderboard'
@@ -12,9 +13,14 @@ def solution():
             pos_i.append(td.get_text())
         pos_i.remove(pos_i[0])
         pos_i[0] = pos_i[0][5:]
+        print(pos_i)
         position.append(pos_i)
     del position[0]
     return position
 
 a = solution()
 print(a)
+
+
+#tips：list添加数据为二维数组：
+#book_list.append([title,rating,people_num,author_info,pub_info])
